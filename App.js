474cd2391d55d5a005600c43e04db3c188f06030
Golamrabbi Azad <golamrabbiazad@gmail.com>
2023-11-10 21:03:31 +0600

    chore: initial commit

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+import PlaceholderImage from "./assets/images/background-image.jpg";
+import ImageViewer from "./components/image-viewer";
+import Button from "./components/button";
+import { useState } from "react";
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("you didn't select any image");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.imageContainer}>
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button
+          label="Choose a photo"
+          theme="primary"
+          onPress={pickImageAsync}
+        />
+        <Button label="Use this photo" />
+      </View>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +47,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#24292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
